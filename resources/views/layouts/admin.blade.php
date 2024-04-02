36a179bbc78446197850b4288a069e6f13e38ddc
Moshiur Rahman <unimrgm@gmail.com>
2024-04-02 17:32:46 +0600

    added tailwindcss into the project

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -22,6 +22,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/css/perfect-scrollbar.min.css" rel="stylesheet" />
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet" />
+    @vite('resources/css/app.css')
     @yield('styles')
 </head>
 
